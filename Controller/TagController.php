7e4b2284a5f5fd35c02acb9a71a0c516f7c35b82
ツ Liverbool <nukboon@gmail.com>
2016-01-22 14:27:29 +0700

    update to 0.17

--- Controller/TagController.php
+++ Controller/TagController.php
@@ -3,6 +3,7 @@
 namespace DoS\TaggingBundle\Controller;
 
 use DoS\ResourceBundle\Controller\ResourceController;
+use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -15,9 +16,10 @@ class TagController extends ResourceController
      */
     public function searchJsonAction(Request $request)
     {
+        $configuration = $this->requestConfigurationFactory->create($this->metadata, $request);
         $keyword = $request->get('keyword');
         $results = $this->get('dos.repository.tag')->search($keyword) ?: array();
 
-        return $this->handleView($this->view($results));
+        return $this->viewHandler->handle($configuration, View::create($results));
     }
 }
