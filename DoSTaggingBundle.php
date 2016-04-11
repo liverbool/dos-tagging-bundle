6a22b0db1934fca4b9d4e4b9e72d2c30e5b1f238
ツ Liverbool <nukboon@gmail.com>
2016-04-12 02:05:23 +0700

    remove interface map

--- DoSTaggingBundle.php
+++ DoSTaggingBundle.php
@@ -6,14 +6,4 @@ use DoS\ResourceBundle\DependencyInjection\AbstractResourceBundle;
 
 class DoSTaggingBundle extends AbstractResourceBundle
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function getModelInterfaces()
-    {
-        return array(
-            'DoS\TaggingBundle\Model\TagInterface' => 'dos.model.tag.class',
-            'DoS\TaggingBundle\Model\TaggingInterface' => 'dos.model.tagging.class',
-        );
-    }
 }
